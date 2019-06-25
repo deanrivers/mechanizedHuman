4d59f919f0c1f876448b0bfc163c50636f766b04
deanrivers <deanriversdeveloper@gmail.com>
2019-06-24 22:26:51 -0400

    Contact Section Update

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,19 +4,21 @@ class Contact extends Component {
     render(){
         return(
             <div id="contact-container">
-                <form id="name">
-                    <input type="text" name="firstname" placeholder="First"></input>
-                    <input type="text" name="lastname" placeholder="Last"></input>
+                <form id="contact-form">
+                    <input id="name-field" className="text-field" type="text" name="firstname" placeholder="Name"></input>
                     <br/>                  
                     <br/>
-                    <input type="email" name="email" placeholder="Email"></input>
+                    <input className="text-field" type="email" name="email" placeholder="Email"></input>
                     <br/>                  
                     <br/>
-                    <input type="message" name="message" placeholder="Message"></input>
+                    <textarea id="message-area" className="text-field" type="message" name="message" placeholder="Message"></textarea>
                     <br/>                  
                     <br/>
-                    <input type="submit"/>
+                    <button id="submit-button">Submit</button>
+                    
                 </form>
+                
+                
                 
             
             </div>
