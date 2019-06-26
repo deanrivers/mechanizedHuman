ce9377a177193bd7e94fd9cd585d2f1d3e2de2ab
deanrivers <deanriversdeveloper@gmail.com>
2019-06-25 21:50:26 -0400

    Contact section update

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
 
+
+
+var max_chars = 160;
+
 class Contact extends Component {
+
+    
+    increment(event){
+        
+        
+        let textLength = event.target.value.length;
+        
+        this.setState({ charactersRemaining: max_chars - textLength});
+        
+
+
+    }
+
+
+    constructor(props){
+        super(props);
+        this.state={
+            charactersRemaining: 160,
+            charatctersEntered: 0,
+        };
+        this.increment = this.increment.bind(this);
+    }
+
+    
+
     render(){
         return(
             <div id="contact-container">
                 <form id="contact-form">
                     <input id="name-field" className="text-field" type="text" name="firstname" placeholder="Name"></input>
-                    <br/>                  
-                    <br/>
+                    
                     <input className="text-field" type="email" name="email" placeholder="Email"></input>
-                    <br/>                  
-                    <br/>
-                    <textarea id="message-area" className="text-field" type="message" name="message" placeholder="Message"></textarea>
-                    <br/>                  
-                    <br/>
+                    
+                    <textarea id="message-area" className="text-field" onKeyUp={this.increment} maxLength="160" type="message" name="message" placeholder="Message..."></textarea>
+                    <span id="character-count">You have {this.state.charactersRemaining} charatcters remaming.</span>
                     <button id="submit-button">Submit</button>
                     
+
+                    
                 </form>
+                
                 
                 
                 
