519df05f63abf41c506e24128ab47f26e97e22e9
Jose Ricardo <jrc.dev3@gmail.com>
2019-06-25 21:04:51 -0400

    correct env variable names

--- serverFunctions/server.js
+++ serverFunctions/server.js
@@ -6,8 +6,8 @@ const app = express();
 const router = express.Router();
 const port = process.env.PORT || 5000;
 
-const consumer_id = process.env.consumer_id
-const consumer_secret = process.env.consumer_secret
+const consumer_id = process.env.CONSUMER_ID
+const consumer_secret = process.env.CONSUMER_SECRET
 
 const Parser = require('rss-parser');
 const parser = new Parser();
@@ -84,8 +84,6 @@ router.get('/fetchPhys', async (req, res) => {
 
 router.get('/fetchMit', async (req, res) => {
   let feed = await fetchRSS('http://news.mit.edu/rss/topic/science-technology-and-society')
-  console.log(feed)
-  //res.send({ data: "tester" });
   res.send(feed);
 });
 
