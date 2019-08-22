de491c10e8ecf2aab9b11cd0a2c18187273b158a
Jose Ricardo <jrc.dev3@gmail.com>
2019-08-22 19:37:15 -0400

    added clickable links in tweets

--- serverFunctions/server.js
+++ serverFunctions/server.js
@@ -25,20 +25,13 @@ const oauth2Client = new google.auth.OAuth2(
   ['http://localhost']
 );
 
-const scopes = [
-  'https://www.googleapis.com/auth/gmail.send',
-  'https://www.googleapis.com/auth/gmail.compose',
-  'https://www.googleapis.com/auth/gmail.readonly',
-];
-
-
 router.post('/sendMail', async (req, res) => {
-    
+
   oauth2Client.setCredentials({
     refresh_token: gmailRefreshToken
   });
 
-  try{
+  try {
 
     const gmail = google.gmail({
       version: 'v1',
@@ -47,37 +40,39 @@ router.post('/sendMail', async (req, res) => {
 
     const makeBody = () => {
       var str = ["Content-Type: text/plain; charset=\"UTF-8\"\n",
-          "MIME-Version: 1.0\n",
-          "Content-Transfer-Encoding: 7bit\n",
-          "to: ", "mechhummedia@gmail.com", "\n",
-          "from: ", "mechhummedia@gmail.com", "\n",
-          "subject: ", "New Notification!", "\n\n",
-          "Sender Email: " + req.body.email + "!\nMessage:\n\n" + req.body.message
+        "MIME-Version: 1.0\n",
+        "Content-Transfer-Encoding: 7bit\n",
+        "to: ", "mechhummedia@gmail.com", "\n",
+        "from: ", "mechhummedia@gmail.com", "\n",
+        "subject: ", "New Notification!", "\n\n",
+        "Sender Email: " + req.body.email + "!\nMessage:\n\n" + req.body.message
       ].join('');
-    
-    let base64EncodedEmail = Base64.encodeURI(str);
-    return base64EncodedEmail
+
+      let base64EncodedEmail = Base64.encodeURI(str);
+      return base64EncodedEmail
     }
 
-    const response = await gmail.users.messages.send({
+    // const response = 
+    await gmail.users.messages.send({
       userId: 'me',
       resource: {
-      raw: makeBody()
-    }})
-    console.log(response)
+        raw: makeBody()
+      }
+    })
+    // console.log(response)
 
-  }catch (error){
+  } catch (error) {
     console.log(error)
   }
   res.send(200)
 })
 
 
- 
+
 // Twitter Timeline Endpoint
 router.get('/fetchTimeline', async (req, res) => {
 
-    // Twitter oauth config
+  // Twitter oauth config
   const twitterCredentials = {
     client: {
       id: consumer_id,
@@ -102,22 +97,74 @@ router.get('/fetchTimeline', async (req, res) => {
     const accessTokenObject = twitterOauth2.accessToken.create(result);
     const accessToken = accessTokenObject.token['access_token']
     const config = {
-      headers: { 
+      headers: {
         'Authorization': 'Bearer ' + accessToken,
         'Content-Type': 'application/json',
         'Accept-Encoding': 'gzip'
       },
     }
 
-    const response = await axios.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=mechanizedhuman&count=6', config)
+    const response = await axios.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=mechanizedhuman&count=20', config)
     const data = await response.data
 
-    myData = data
+    parsedTweets = []
+
+    data.forEach(tweet => {
+      let date = new Date(tweet['created_at'])
+
+      const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"
+      ];
+
+      let parsedHour = ''
+      let parsedSuffix = 'AM'
+
+      if (date.getHours() == 12 || date.getHours() == 0 ) {
+        parsedHour = 12
+        parsedSuffix = date.getHours() == 12 ? 'PM':'AM'
+      } else if (date.getHours() > 12) {
+        parsedHour = date.getHours() - 12
+        parsedSuffix = 'PM'
+      } else {
+        parsedHour = date.getHours()
+      }
+
+      const timestamp =
+        date.getDate() + ' '
+        + monthNames[date.getMonth()] + ' '
+        + date.getFullYear()
+        + ' @ ' + parsedHour
+        + ':' + (date.getMinutes() > 10 ? date.getMinutes() : '0' + date.getMinutes())
+        + parsedSuffix
+
+      let parsedText = ''
+      let href = ''
+      let textArray = tweet['text'].split(' ')
+
+      textArray.forEach(phrase => {
+
+        const pattern = RegExp('^http')
+        if (pattern.test(phrase)) {
+          href = phrase
+        } else {
+          parsedText += phrase + ' '
+        }
+      })
+
+      const parsedObject = {
+        text: parsedText,
+        href,
+        timestamp
+      }
+
+      parsedTweets.push(parsedObject)
+    })
+
+    myData = parsedTweets
 
   } catch (error) {
     console.log('Access Token error', error.message);
   }
-
   res.send(myData);
 });
 
@@ -130,7 +177,7 @@ const fetchRSS = async (url) => {
 router.get('/fetchFeed', async (req, res) => {
   let physFeed = await fetchRSS('https://phys.org/rss-feed/breaking/technology-news/')
   let mitFeed = await fetchRSS('http://news.mit.edu/rss/topic/science-technology-and-society')
-  let feed = {items: [...physFeed['items'], ...mitFeed['items']]}
+  let feed = { items: [...physFeed['items'], ...mitFeed['items']] }
 
   res.send(feed);
 });
@@ -144,6 +191,7 @@ router.get('/fetchMit', async (req, res) => {
   let feed = await fetchRSS('http://news.mit.edu/rss/topic/science-technology-and-society')
   res.send(feed);
 });
+
 
 app.use('/.netlify/functions/server', router);
 
